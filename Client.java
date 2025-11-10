96caed035e324578b229713aee2337e9ac343996
Briana <bsisler81@gmail.com>
2025-11-10 14:26:57 -0500

    Created skeleton for Client and Server

--- Client.java
+++ Client.java
@@ -1,3 +1,21 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
 public class Client {
-    
+    public Client (String ip, int port) {
+        
+    }
+
+    public class Handshake {
+
+    }
+
+    public class Request {
+
+    }
+
+    public class Disconnect {
+
+    }
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,25 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
 public class Server {
-    
+    private class Clients {
+
+    }
+
+    public class Serve {
+
+    }
+
+    public class Run {
+
+    }
+
+    public class Disconnect {
+
+    }
+
+    public class GetConectedTimes {
+        
+    }
 }
